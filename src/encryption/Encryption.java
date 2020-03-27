2d7831d76a087a183aebaed5dd2244b2b5404772
Mia Bendy <miabendy@Mias-MacBook-Pro.local>
2020-03-27 10:59:45 -0500

    setting up files for encryption ticket

---
+++ src/encryption/Encryption.java
@@ -0,0 +1,10 @@
+package encryption;
+
+public class Encryption {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/encryptionTest/EncryptionTest.java
@@ -0,0 +1,14 @@
+package encryptionTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EncryptionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
