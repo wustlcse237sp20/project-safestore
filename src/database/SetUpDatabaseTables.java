62be17b3069ff30d3e3e7007b635a39221cc09cd
Ryan Loutos <ryanloutos@Ryans-MacBook-Pro.local>
2020-03-27 15:35:41 -0500

    fixed database with foreign keys

--- src/database/SetUpDatabaseTables.java
+++ src/database/SetUpDatabaseTables.java
@@ -2,6 +2,7 @@ package database;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
@@ -14,17 +15,25 @@ import tables.User;
 import tables.WebsiteAccount;
 
 public class SetUpDatabaseTables {
-
+	
+	// this will print out the create table query statements without the foreign key constraints
+	//	due to lack of functionality. You need to add the foreign keys as necessary if you want
+	// 	them in the database for SQLite
 	public static void main(String[] args) {
 		String databaseUrl = "jdbc:sqlite:src/database/app.db";
 		ConnectionSource connectionSource;
 		try {
 			connectionSource = new JdbcConnectionSource(databaseUrl);
-			TableUtils.createTable(connectionSource, User.class);
-			TableUtils.createTable(connectionSource, Address.class);
-			TableUtils.createTable(connectionSource, WebsiteAccount.class);
-			TableUtils.createTable(connectionSource, CreditCard.class);
-			TableUtils.createTable(connectionSource, DebitCard.class);
+			List<String> userTableClassCreation = TableUtils.getCreateTableStatements(connectionSource, User.class);
+			List<String> addressTableClassCreation = TableUtils.getCreateTableStatements(connectionSource, Address.class);
+			List<String> websiteAccountTableClassCreation = TableUtils.getCreateTableStatements(connectionSource, WebsiteAccount.class);
+			List<String> creditCardTableClassCreation = TableUtils.getCreateTableStatements(connectionSource, CreditCard.class);
+			List<String> debitCardTableClassCreation = TableUtils.getCreateTableStatements(connectionSource, DebitCard.class);
+			System.out.println(userTableClassCreation);
+			System.out.println(addressTableClassCreation);
+			System.out.println(websiteAccountTableClassCreation);
+			System.out.println(creditCardTableClassCreation);
+			System.out.println(debitCardTableClassCreation);
 			connectionSource.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
