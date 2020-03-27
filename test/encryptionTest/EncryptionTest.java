8c3f14cd298ea8d156ea7503c88249a9f150abf6
Mia Bendy <miabendy@Mias-MacBook-Pro.local>
2020-03-27 13:37:48 -0500

    added initial test for encryption.

--- test/encryptionTest/EncryptionTest.java
+++ test/encryptionTest/EncryptionTest.java
@@ -2,13 +2,22 @@ package encryptionTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import encryption.Encryption;
 
 class EncryptionTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testEncryptAndDecrypt() {
+		String [] originalStrings = {"testing", "234", "de23s", "!@#kdf", "20394705029384"};
+		for (String stringToEncrypt : originalStrings) {
+			String encryptedString = Encryption.encrypt(stringToEncrypt);
+			String decryptedString = Encryption.decrypt(encryptedString);
+			String failureMessage = "Original String: " + stringToEncrypt + " does not match Decrypted String: " + decryptedString;
+			Assert.assertEquals(failureMessage, stringToEncrypt, decryptedString);
+		}
+		
 	}
 
 }
