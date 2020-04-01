d1ed793a1ccb69daa633d5951a795604e5af28de
Ryan Loutos <ryanloutos@Ryans-MacBook-Pro.local>
2020-04-01 15:25:53 -0500

    WebsiteAccount class made with insert. Needs approval

--- src/websiteAccount/WebsiteAccount.java
+++ src/websiteAccount/WebsiteAccount.java
@@ -4,44 +4,41 @@ import java.sql.SQLException;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 
 import encryption.Encryption;
-import tables.UserEntity;
 import tables.WebsiteAccountEntity;
+import user.User;
 
 public class WebsiteAccount {
 	
-	private WebsiteAccountEntity websiteAccount;
+	private WebsiteAccountEntity websiteAccountEntity;
 	
-	//TODO: change UserEntity to User once Tali finished
-	public WebsiteAccount(UserEntity safeStoreUser, String nickname, String websiteLogin, 
-			String websitePassword) throws SQLException {
-			websiteAccount = new WebsiteAccountEntity(safeStoreUser, Encryption.encrypt(nickname), 
+	public WebsiteAccount(User safeStoreUser, String nickname, String websiteLogin, String websitePassword) {
+			websiteAccountEntity = new WebsiteAccountEntity(safeStoreUser.getUserEntity(), Encryption.encrypt(nickname), 
 					Encryption.encrypt(websiteLogin), Encryption.encrypt(websitePassword));
 	}
 	
-	public WebsiteAccountEntity getWebsiteAccount() {
-		return websiteAccount;
-	}
-
-	public void setWebsiteAccount(WebsiteAccountEntity websiteAccount) {
-		this.websiteAccount = websiteAccount;
+	public WebsiteAccount(WebsiteAccountEntity websiteAccountEntity) {
+		this.websiteAccountEntity = websiteAccountEntity;
 	}
 	
-	public void addWebsiteAccount(ConnectionSource connectionSource) throws SQLException {
+	public WebsiteAccountEntity getWebsiteAccountEntity() {
+		return this.websiteAccountEntity;
+	}
+
+	public void setWebsiteAccountEntity(WebsiteAccountEntity websiteAccount) {
+		this.websiteAccountEntity = websiteAccount;
+	}
+	
+	public void addWebsiteAccount(ConnectionSource databaseConnection) throws SQLException {
 		try {
 			Dao<WebsiteAccountEntity, Integer> websiteAccountDao = 
-					DaoManager.createDao(connectionSource, WebsiteAccountEntity.class);
-			websiteAccountDao.create(websiteAccount);
+					DaoManager.createDao(databaseConnection, WebsiteAccountEntity.class);
+			websiteAccountDao.create(websiteAccountEntity);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public static void main(String[] args) {
-
 	}
 
 }
--- test/queryWithDatabaseTests/WebsiteAccountTests.java
+++ test/queryWithDatabaseTests/WebsiteAccountTests.java
@@ -16,11 +16,12 @@ import com.j256.ormlite.support.ConnectionSource;
 import encryption.Encryption;
 import tables.UserEntity;
 import tables.WebsiteAccountEntity;
+import user.User;
 import websiteAccount.WebsiteAccount;
 
 class WebsiteAccountTests {
 	
-	ConnectionSource connectionSource;
+	ConnectionSource databaseConnection;
 	String testUserUsername = "username";
 	String testUserPassword = "password";
 	String testUserSalt = "salt";
@@ -30,41 +31,50 @@ class WebsiteAccountTests {
 	String testAccountPassword = "pA$$woRd";
 	WebsiteAccountEntity testWebsiteAccountEntity = 
 			new WebsiteAccountEntity(testUserEntity, testAccountNickname, testAccountLogin, testAccountPassword);
+	Dao<UserEntity, String> userDao;
 	Dao<WebsiteAccountEntity, Integer> websiteAccountDao;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		try {
-			this.connectionSource = new JdbcConnectionSource("jdbc:sqlite:src/database/app.db");
-			assertNotNull(connectionSource, "Connection is null. Failed to Connect.");
-			websiteAccountDao = DaoManager.createDao(connectionSource, WebsiteAccountEntity.class);
+			this.databaseConnection = new JdbcConnectionSource("jdbc:sqlite:src/database/app.db");
+			assertNotNull(databaseConnection, "Connection is null. Failed to Connect.");
+			websiteAccountDao = DaoManager.createDao(databaseConnection, WebsiteAccountEntity.class);
 		} catch (SQLException e) {
 			fail("Failed to connect to database and set up DAO.");
 			e.printStackTrace();
 		}
 	}
 
-	// TODO: Need to put the user object in database to reference for foreign keys
 	@Test
 	void testInsertWebsiteAccountToDatabase() {
 		try {
 			// insert both into database both ways
+			User testUser = new User(testUserEntity);
+			testUser.createSafeStoreAccount(databaseConnection);
 			websiteAccountDao.create(testWebsiteAccountEntity);
+			
 			WebsiteAccount websiteAccountQuery = // insert query object into database
-					new WebsiteAccount(testUserEntity, testAccountNickname, testAccountLogin, testAccountPassword, connectionSource);
-			websiteAccountQuery.insertIntoDatabase();
+					new WebsiteAccount(testUser, testAccountNickname, testAccountLogin, testAccountPassword);
+			websiteAccountQuery.addWebsiteAccount(databaseConnection);
+			
 			// pull from database both objects and make sure they are equal
 			WebsiteAccountEntity testEntityPulledFromDb = websiteAccountDao.queryForId(testWebsiteAccountEntity.getId());
-			WebsiteAccountEntity queryEntityPulledFromDb = websiteAccountDao.queryForId(websiteAccountQuery.getWebsiteAccount().getId());
-			websiteAccountDao.delete(testEntityPulledFromDb); // delete rows from db once pulled
-			websiteAccountDao.delete(queryEntityPulledFromDb);
-			websiteAccountDao.refresh(testEntityPulledFromDb);
+			WebsiteAccountEntity queryEntityPulledFromDb = websiteAccountDao.queryForId(websiteAccountQuery.getWebsiteAccountEntity().getId());
 			System.out.println(testEntityPulledFromDb.getSafeStoreUser());
-//			assertEquals("Usernames foreign keys not equal", 
-//					testEntityPulledFromDb.getSafeStoreUser().getUsername(), queryEntityPulledFromDb.getSafeStoreUser().getUsername());
+			assertEquals("Usernames foreign keys not equal", 
+					testEntityPulledFromDb.getSafeStoreUser().getUsername(), queryEntityPulledFromDb.getSafeStoreUser().getUsername());
 			assertEquals("Nicknames not equal", testEntityPulledFromDb.getNickname(), Encryption.decrypt(queryEntityPulledFromDb.getNickname()));
 			assertEquals("Logins not equal", testEntityPulledFromDb.getWebsiteLogin(), Encryption.decrypt(queryEntityPulledFromDb.getWebsiteLogin()));
 			assertEquals("Passwords not equal", testEntityPulledFromDb.getWebsitePassword(), Encryption.decrypt(queryEntityPulledFromDb.getWebsitePassword()));
+			
+			// delete rows from database once completed test
+			websiteAccountDao.delete(testEntityPulledFromDb); 
+			websiteAccountDao.delete(queryEntityPulledFromDb);
+			
+			// Need to delete user manually because delete not working for some reason
+			//userDao.delete(testUser.getUserEntity());
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} 
