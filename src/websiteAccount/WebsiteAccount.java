910386d29ce9e6720294ca7ca53314b2da436254
Ryan Loutos <ryanloutos@Ryans-MacBook-Pro.local>
2020-04-02 10:47:15 -0500

    added static prompting class to add website account, needs testing

--- src/websiteAccount/WebsiteAccount.java
+++ src/websiteAccount/WebsiteAccount.java
@@ -1,9 +1,11 @@
 package websiteAccount;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 
 import encryption.Encryption;
@@ -69,5 +71,57 @@ public class WebsiteAccount {
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * Prompts the user to add info for a website account 
+	 * @param databaseConnection - the ConnectionSource object to the database where the 
+	 * 								the web account will be stored
+	 * @param keyboard - the input stream for entering in info which will be the keyboard
+	 */
+	public static void addWebsiteAccountPrompts(ConnectionSource databaseConnection, Scanner keyboard) {
+		// Setting up website account variables
+		String nickname = "";
+		String login = "";
+		String password = "";
+		
+		System.out.println("Please provide a name for this account: ");
+		nickname = keyboard.nextLine();
+		System.out.println("Please provide the login (username or email adddress) for this account: ");
+		login = keyboard.nextLine();
+		System.out.println("Please provide the password for this account: ");
+		password = keyboard.nextLine();
+		
+		User safeStoreUser = new User("testUser", "testPassword");
+		WebsiteAccount websiteAccount = new WebsiteAccount(safeStoreUser, nickname, login, password);
+		try {
+			websiteAccount.addWebsiteAccount(databaseConnection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		ConnectionSource databaseConnection;
+		String databaseUrl = "jdbc:sqlite:src/database/app.db";
+		try {
+			databaseConnection = new JdbcConnectionSource(databaseUrl);
+			Scanner keyboard = new Scanner(System.in);
+			System.out.println("Would you like to add a Website Account, Credit Card, or Debit Card?");
+			String userInput = keyboard.nextLine();
+			while (!userInput.trim().equals("Website Account") && !userInput.trim().equals("Credit Card") && !userInput.trim().equals("Debit Card")) {
+				System.out.println("Enter 'Website Account', 'Credit Card', or 'Debit Card'");
+		        userInput = keyboard.nextLine();
+		    }
+			if (userInput.equals("Website Account")) {
+				WebsiteAccount.addWebsiteAccountPrompts(databaseConnection, keyboard);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 }
